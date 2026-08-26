2a4af31147cb1a3c6316b6e01faaefb1a4a79e1b
VeljkoS01 <veljko.simonovic@elfak.rs>
2026-08-26 13:13:21 +0200

    application health endpoint

--- src/IksOks.Web/Program.cs
+++ src/IksOks.Web/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/health", () =>
+{
+    return Results.Ok(new
+    {
+        status = "ok",
+        application = "IksOks"
+    });
+});
 
 app.Run();
