421cfc6df0346cb9a21b29753cfc5fe591ff9191
VeljkoS01 <veljko.simonovic@elfak.rs>
2026-08-27 12:28:51 +0200

    add missing EF Core DbContext implementation

--- src/IksOks.Web/Infrastructure/Persistence/IksOksDbContext.cs
+++ src/IksOks.Web/Infrastructure/Persistence/IksOksDbContext.cs
@@ -1,6 +1,41 @@
-﻿namespace IksOks.Web.Infrastructure.Persistence
+﻿using IksOks.Web.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IksOks.Web.Infrastructure.Persistence;
+
+public sealed class IksOksDbContext : DbContext
 {
-    public class IksOksDbContext
+    public IksOksDbContext(DbContextOptions<IksOksDbContext> options)
+        : base(options)
     {
+    }
+
+    public DbSet<AppUser> Users => Set<AppUser>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var user = modelBuilder.Entity<AppUser>();
+
+        user.ToTable("Users");
+
+        user.HasKey(x => x.Id);
+
+        user.Property(x => x.UserName)
+            .HasMaxLength(32)
+            .IsRequired();
+
+        user.Property(x => x.NormalizedUserName)
+            .HasMaxLength(32)
+            .IsRequired();
+
+        user.HasIndex(x => x.NormalizedUserName)
+            .IsUnique();
+
+        user.Property(x => x.PasswordHash)
+            .HasMaxLength(512)
+            .IsRequired();
+
+        user.Property(x => x.CreatedAt)
+            .IsRequired();
     }
 }
--- src/IksOks.Web/Program.cs
+++ src/IksOks.Web/Program.cs
@@ -1,4 +1,16 @@
+using IksOks.Web.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<IksOksDbContext>(options =>
+{
+    var connectionString =
+        builder.Configuration.GetConnectionString("Default");
+
+    options.UseNpgsql(connectionString);
+});
+
 var app = builder.Build();
 
 app.MapGet("/api/health", () =>
