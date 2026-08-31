8a3d230fcf8bee07a18db2134d7d9190854a6f59
K1m1X <misapavlovic2@gmail.com>
2026-08-31 14:28:03 +0200

    match persistence model

---
+++ src/IksOks.Web/Domain/Entities/GameMatch.cs
@@ -0,0 +1,26 @@
+﻿using IksOks.Web.Domain.Enums;
+
+namespace IksOks.Web.Domain.Entities;
+
+public sealed class GameMatch
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Guid OwnerUserId { get; set; }
+
+    public AppUser OwnerUser { get; set; } = null!;
+
+    public Guid? OpponentUserId { get; set; }
+
+    public AppUser? OpponentUser { get; set; }
+
+    public int BoardSize { get; set; }
+
+    public int WinLength { get; set; }
+
+    public MatchStatus Status { get; set; }
+        = MatchStatus.WaitingForOpponent;
+
+    public DateTimeOffset CreatedAt { get; set; }
+        = DateTimeOffset.UtcNow;
+}
