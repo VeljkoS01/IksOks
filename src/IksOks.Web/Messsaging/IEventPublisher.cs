c63d189f5cb3b667ec16c91155efd2d5c3aa7f90
VeljkoS01 <veljko.simonovic@elfak.rs>
2026-09-04 13:20:15 +0200

    rabbitmq infrastructure

---
+++ src/IksOks.Web/Messsaging/IEventPublisher.cs
@@ -0,0 +1,10 @@
+﻿using IksOks.Web.Messaging.Contracts;
+
+namespace IksOks.Web.Messaging;
+
+public interface IEventPublisher
+{
+    Task PublishMatchFinishedAsync(
+        MatchFinishedEvent message,
+        CancellationToken cancellationToken);
+}
