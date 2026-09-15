4e265d0fe9c26704eb52673af01d330501002179
VeljkoS01 <veljko.simonovic@elfak.rs>
2026-09-15 13:33:25 +0200

    match state model

---
+++ src/IksOks.Web/Domain/States/IMatchState.cs
@@ -0,0 +1,17 @@
+﻿using IksOks.Web.Domain.Entities;
+using IksOks.Web.Domain.Enums;
+
+namespace IksOks.Web.Domain.States;
+
+public interface IMatchState
+{
+    MatchStatus Status { get; }
+
+    bool CanJoin(GameMatch match);
+
+    bool CanMakeMove(GameMatch match);
+
+    MatchStatus OnOpponentJoined();
+
+    MatchStatus OnGameFinished();
+}
